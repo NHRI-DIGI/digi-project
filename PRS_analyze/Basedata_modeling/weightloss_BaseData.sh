7bcfaa00725fc403737d5192672014c775c5f49c
CHENG-KH <69302962+CHENG-KH@users.noreply.github.com>
2022-07-30 21:58:29 +0800

    for digit use

---
+++ PRS_analyze/Basedata_modeling/weightloss_BaseData.sh
@@ -0,0 +1,168 @@
+# Build output route, and the pheno name -> phenoarray
+# Merge VCF in merge_dir
+# tool in ${pathway}Input/Script/bin/
+# case and control txt file in ${pathway}Input/Laptop_input/
+### Important!!!! Make sure the chr = 1 or chr1
+pathway='/staging2/reserve/flagship/u3121714/kenny/'
+phenoarray=weightloss_12
+merge_dir='/staging2/reserve/flagship/u3121714/kenny/Input/BaseData/hg38/weightloss/VCF_merge/'
+
+#===========================================================
+# Input case and contol txt
+data=${pathway}Input/Laptop_input/weightloss_base.txt
+# Classify control and case, 2 -> case，1 -> control; Due to manage the effect allele, so choose the oposite to set up
+control=($(awk '$3 == 2 {printf $1" "}' $data))
+case=($(awk '$3 == 1 {printf $1" "}' $data))
+
+cd ${pathway}Output
+mkdir Laptop_output
+cd ${pathway}Output/Laptop_output
+mkdir OutputImage  OutputImage/QCplot
+cd ${pathway}Output
+mkdir Basedata
+
+cd ${pathway}Output/Basedata
+mkdir $phenoarray process
+mkdir $phenoarray/ef_al_process
+
+# Make Heaader
+cat ${merge_dir}"$phenoarray".merge.vcf | egrep "^##" > process/"$phenoarray"_Headers.txt
+sed '/^##/d' ${merge_dir}"$phenoarray".merge.vcf > process/"$phenoarray".vcf
+head -n1 process/"$phenoarray".vcf > process/"$phenoarray"_Header_2.txt
+
+# CHR: X->23 Y->24 M->25 (avoid Invalid variant bp coordinate ERROR afterward)
+awk -F'\t' -vOFS='\t' '/^#/ {next}; {gsub("X", "23", $1) ; gsub("Y", "24", $1); gsub("M", "25", $1); print}' process/"$phenoarray".vcf > process/noh_xym_"$phenoarray".vcf
+cat process/"$phenoarray"_Header_2.txt process/noh_xym_"$phenoarray".vcf > process/xym_"$phenoarray".vcf
+
+# Replace ID to chr_POS, Remove Duplicate SNPs
+awk 'BEGIN{OFS="_";}  NR==1{print $2} NR >1{print $1,$2}' process/xym_"$phenoarray".vcf > process/chr_pos
+# If original vcf file chr column = chr1... must do the code below.
+# If not, don't do it.
+sed -e 's/^chr//' -i process/chr_pos
+#######################################
+awk 'FNR==NR{a[NR]=$0;next} {$3=a[FNR]}1' OFS="\t" process/chr_pos  process/xym_"$phenoarray".vcf > process/noh_chr_xym_"$phenoarray".vcf
+( sed 1q process/"$phenoarray"_Header_2.txt; sed 1d process/noh_chr_xym_"$phenoarray".vcf ) > process/chr_xym_"$phenoarray".vcf
+(head -n 1 process/chr_xym_"$phenoarray".vcf && tail -n +2 process/chr_xym_"$phenoarray".vcf | sort -u  -k3,3 ) > process/nodup_chr_xym_"$phenoarray".vcf
+
+# Indiviual QC
+${pathway}Input/Script/bin/plink --vcf process/nodup_chr_xym_"$phenoarray".vcf --make-bed --out process/indQC
+${pathway}Input/Script/bin/plink --bfile process/indQC --missing --out process/miss_indQC
+${pathway}Input/Script/bin/plink --bfile process/indQC --het --out process/het_indQC
+Rscript ${pathway}Input/Script/indQC.r
+cp process/miss_indQC.imiss ../../Input/Laptop_input/
+mv ../../Input/Laptop_input/miss_indQC.imiss ../../Input/Laptop_input/base_miss_indQC.imiss
+cp process/het_indQC.het ../../Input/Laptop_input/
+mv ../../Input/Laptop_input/het_indQC.het ../../Input/Laptop_input/base_het_indQC.het
+cp process/fail_imiss_ind.txt ../../Input/Laptop_input/
+mv ../../Input/Laptop_input/fail_imiss_ind.txt ../../Input/Laptop_input/base_fail_imiss_ind.txt
+cp process/fail_het_ind.txt ../../Input/Laptop_input/
+mv ../../Input/Laptop_input/fail_het_ind.txt ../../Input/Laptop_input/base_fail_het_ind.txt
+cat process/fail_het_ind.txt  process/fail_imiss_ind.txt | sort -k1 | uniq > fail_indQC.txt
+${pathway}Input/Script/bin/plink --vcf process/nodup_chr_xym_"$phenoarray".vcf --remove fail_indQC.txt --make-bed  --out process/indQC_nodup_chr_xym_"$phenoarray"
+${pathway}Input/Script/bin/plink --bfile process/indQC_nodup_chr_xym_"$phenoarray" --recode vcf-iid --out process/indQC_nodup_chr_xym_"$phenoarray"
+sed -i '/^##/d' process/indQC_nodup_chr_xym_"$phenoarray".vcf
+
+# Add phenotype info (Every pheno output call "$phenoarray"_p)
+${pathway}Input/Script/bin/plink --vcf process/indQC_nodup_chr_xym_"$phenoarray".vcf --pheno ${pathway}Input/Laptop_input/weightloss_base.txt --pheno-name $phenoarray --make-bed --out $phenoarray/"$phenoarray"_p
+
+# SNP QC (Do only one pheno)
+${pathway}Input/Script/bin/plink \
+    --bfile $phenoarray/"$phenoarray"_p \
+    --allow-no-sex \
+    --maf 0.01 \
+    --hwe 1e-6 \
+    --geno 0.01 \
+    --write-snplist \
+    --make-just-fam \
+    --out snpQC_base
+
+# Export SNP QC report
+preQC=$(< "process/chr_xym_"$phenoarray".vcf" wc -l )
+nodup=$(< "process/nodup_chr_xym_"$phenoarray".vcf" wc -l )
+sQC=$(< "snpQC_base.snplist" wc -l )
+echo $preQC $nodup $sQC > process/snpQC_rowN.txt
+Rscript ${pathway}Input/Script/snpQCreport_base.r
+
+#Association Test
+${pathway}Input/Script/bin/plink --bfile $phenoarray/"$phenoarray"_p --assoc --allow-no-sex --out $phenoarray/chi
+
+# Remove Ambiguous SNPs
+awk '!( ($4=="A" && $7=="T") || \
+        ($4=="T" && $7=="A") || \
+        ($ 4=="G" && $7=="C") || \
+        ($4=="C" && $7=="G")) {print}' $phenoarray/chi.assoc > $phenoarray/ambRm_chi.assoc
+
+# Effect allele Adjustment
+cd $phenoarray
+${pathway}Input/Script/bin/plink --bfile "$phenoarray"_p --recode vcf-iid --out ef_al_process/"$phenoarray"_p
+cd ef_al_process
+
+# Grep first row
+sed -i '/^##/d' "$phenoarray"_p.vcf
+head -n 1 "$phenoarray"_p.vcf > "$phenoarray"_p.txt
+
+# Make control.vcf
+declare -a myArray
+myArray=(`cat "$phenoarray"_p.txt`)
+declare -a num
+num=()
+
+for j in "${!control[@]}"
+do
+  for i in "${!myArray[@]}"
+  do
+    if [ "${control[$j]}" == "${myArray[$i]}" ]
+    then
+      num+=(`expr $i + 1`)
+    fi
+  done
+done
+
+num_complete=$(echo ${num[@]} | tr ' ' ',')
+
+cut --fields="$num_complete" --complement ${pathway}Output/Basedata/$phenoarray/ef_al_process/"$phenoarray"_p.vcf > ${pathway}Output/Basedata/$phenoarray/ef_al_process/cont.vcf
+
+# Make case.vcf
+declare -a num_1
+num_1=()
+
+for j in "${!case[@]}"
+do
+  for i in "${!myArray[@]}"
+  do
+    if [ "${case[$j]}" == "${myArray[$i]}" ]
+    then
+      num_1+=(`expr $i + 1`)
+    fi
+  done
+done
+
+num_complete_1=$(echo ${num_1[@]} | tr ' ' ',')
+
+cut --fields="$num_complete_1" --complement ${pathway}Output/Basedata/$phenoarray/ef_al_process/"$phenoarray"_p.vcf > ${pathway}Output/Basedata/$phenoarray/ef_al_process/case.vcf
+
+# Make freq_case and freq_cont
+${pathway}Input/Script/bin/plink --vcf cont.vcf --make-bed --out cont
+${pathway}Input/Script/bin/plink --vcf case.vcf --make-bed --out case
+${pathway}Input/Script/bin/plink --bfile cont --freq --out freq_cont
+${pathway}Input/Script/bin/plink --bfile case --freq --out freq_case
+
+# Extract 'POS' 'MAF'(of control&case), change these two header'MAF' into 'CONTROL_MAF'&'CASE_MAF'
+awk '{print$1"\t"$2}' "$phenoarray"_p.vcf > pos_"$phenoarray"_p.txt
+awk '{print$5}' freq_cont.frq > cont.txt
+sed -i 's/MAF/CONTROL_MAF/g' cont.txt
+awk '{print$5}' freq_case.frq > case.txt
+sed -i 's/MAF/CASE_MAF/g' case.txt
+
+# Merge 'POS','CASE_MAF','CONTROL_MAF'
+paste pos_"$phenoarray"_p.txt case.txt cont.txt > ef.txt
+
+# Find out which POS is control>case
+awk '$4>$3{print$2}' ef.txt > efp_ct.txt
+perl ${pathway}Input/Script/ef_ad.pl #otput: ef_ambRm_chi.assoc
+cd ../../
+
+# Add log(OR) column (BaseData)
+awk '{s=(NR==1)?"logOR":log($10);$0=$0 OFS s}1' $phenoarray/ef_ambRm_chi.assoc > $phenoarray/finalBase.txt
+cp -r $phenoarray/ef_ambRm_chi.assoc ../finalBase_OR.txt
+sed -i 's|-inf|NA|g' $phenoarray/finalBase.txt
---
+++ PRS_analyze/PRS_analyze/weightloss_bestPvalPRS.R
@@ -0,0 +1,161 @@
+# 註解掉的「程式碼」都可無視
+library(data.table)
+library(magrittr)
+library(tidyverse)
+library(ggpubr)
+library(ggplot2)
+library(dplyr)
+library(stats)
+library(matrixStats)
+
+setwd('/Users/kenny/Desktop/Laptop_input')
+
+urPhen <- "weightloss" # Type phenotype here
+phenotype <- read.table("/Users/kenny/Desktop/Laptop_input/weightloss_target.txt", header=T) %>%
+  mutate(weightloss = weightloss - 1)
+
+# Scree plot -> choose principle componets #################
+tiff(filename = "/Users/kenny/Desktop/Laptop_output/screeplot.tiff",height=6, width=8,units='in', res=600)
+  eigenval <- read.table("/Users/kenny/Desktop/Laptop_input/pcaOut.eigenval")$V1
+  plot(
+    x = seq(1:length(eigenval)), y = eigenval,
+    type = "o",
+    xlab = "Principle Component", ylab = "Variance")
+dev.off()
+#####################
+pcs <- fread("/Users/kenny/Desktop/Laptop_input/pcaOut.eigenvec", header=F) %>%
+    setnames(., colnames(.), c("FID", "IID", paste0("PC",1:12))) # choose all PCs
+pheno <- phenotype %>%
+  merge(., pcs)
+colnames(pheno)[3] <- 'mypheno'
+
+#Choose 2 PC due to the scree plot#####
+pheno <- pheno[,c(1:14)] # 11 PC
+
+# linear model 改用 logistic regression model
+# 人口組成（PCs）和pheno相關性
+#pheno[,!colnames(pheno)%in%c("FID","IID")]
+null.r2 <- summary(lm(mypheno~., data=pheno[,!colnames(pheno)%in%c("FID","IID")]))$r.squared
+prs.result <- NULL
+p.threshold <- c(0.001,0.005,0.01,0.05,0.1,0.2,0.3,0.4,0.5)
+for(i in p.threshold){
+  pheno.prs <- paste0('/Users/kenny/Desktop/Laptop_input/FinalResults/',urPhen,"/pval_th.", i, ".profile") %>%
+    fread(.) %>%
+    .[,c("FID", "IID", "SCORE")] %>%
+    merge(., pheno, by=c("FID", "IID"))
+  pheno.prs<-data.frame(pheno.prs)
+  # prs分數跟pheno相關性
+  #model<-glm(mypheno~.,family=binomial(link="logit"), data=pheno.prs[,!colnames(pheno)%in%c("FID","IID")])%>% summary
+  model<-glm(mypheno~.,family=binomial(link="logit"), data=pheno.prs[,!colnames(pheno)%in%c("FID","IID")])
+  nullmod <- glm(mypheno~1,family=binomial(link="logit"), data=pheno.prs[,!colnames(pheno)%in%c("FID","IID")])
+
+  1-logLik(model)/logLik(nullmod)
+  model.r2 <- 1-logLik(model)/logLik(nullmod)
+  prs.r2 <- model.r2-null.r2
+  #prs.coef <- model$coeff["SCORE",]
+  prs.coef <- summary(model)$coeff["SCORE",]
+  prs.result %<>% rbind(.,
+                        data.frame(Threshold=i, R2=prs.r2,
+                                   P=as.numeric(prs.coef[4]),
+                                   BETA=as.numeric(prs.coef[1]),
+                                   SE=as.numeric(prs.coef[2])))
+}
+print(prs.result[which.max(prs.result$R2),])
+bestCutOff <- prs.result[which.max(prs.result[-1,2])+1,]$Threshold # 不考慮0.001
+prs.result
+
+write.table(paste(urPhen,bestCutOff), file=paste0("/Users/kenny/Desktop/Laptop_output/",urPhen,'_BestPval.txt'), sep="\t", quote=F,row.names=F, col.names=F)
+
+# prs density plot (雙峰圖)
+res <- read.table(paste0('/Users/kenny/Desktop/Laptop_input/FinalResults/',urPhen,"/pval_th.",bestCutOff,".profile"), header=T)
+p <- phenotype
+p2 <- select(p,FID,IID,urPhen)
+colnames(p2)[3] <-'mypheno'
+da <- merge(res,p2,by = 'FID')
+da$Standardized_PRS_Score <- scale(da$SCORE)
+
+da1 <- replace(da, da==0,'Control')
+da2 <- replace(da1, da1==1,'Case')
+da2$urPhen <- factor(da2$mypheno)
+
+gd<-ggdensity(da2, x = "Standardized_PRS_Score",
+   add = "mean", rug = TRUE,
+   color = "urPhen", fill = "urPhen",
+   palette = c("#FC4E07", "#0073C2FF"))+theme(legend.title=element_blank()) + ggtitle(urPhen)
+ggsave(plot=gd, filename=paste0("/Users/kenny/Desktop/Laptop_output/OutputImage/",urPhen,"_DensityPlot.tiff"), height = 4, width = 6)
+
+
+## ---------------------------------------------------------------------------------------------#
+# --------------------------------------------------------------------------------------------- #
+# Export indQC plot
+imiss <- read.table("/Users/kenny/Desktop/Laptop_input/base_miss_indQC.imiss", header = T, sep = "")
+het <- read.table("/Users/kenny/Desktop/Laptop_input/base_het_indQC.het", header = T, sep ="")
+
+##visulize individals fail imiss and het
+het$Het_propo = (het$N.NM. - het$O.HOM.)/het$N.NM.
+imiss_het = merge(het,imiss,by = "FID")
+###make a plot
+Addcolors = densCols(imiss_het$F_MISS,imiss_het$Het_propo)
+
+tiff(filename = '/Users/kenny/Desktop/Laptop_output/OutputImage/QCplot/indQCplot_base.tiff',height=6, width=8,units='in', res=600)
+
+indQCplot<-plot(imiss_het$F_MISS,imiss_het$Het_propo, col=Addcolors,pch=20,
+     xlim=c(0,0.06),
+     ylim=c(0.18,0.212),
+     #ylim=c(0.1,0.35),
+     xlab="Missing rate", ylab="Heterozygosity rate", axes=FALSE, cex.lab=1.3, cex.axis=1.3, cex.main=1.3)
+axis(2,tick=T) #2, left. t, tickmarks
+axis(1,tick=T) #1, below
+legend("bottomright", legend="Genotypes_missing_het", pch=16, bty="n", col = Addcolors)
+
+abline(h=mean(imiss_het$Het_propo)-
+         (3*sd(imiss_het$Het_propo)),col="RED",lty=2)
+abline(h=mean(imiss_het$Het_propo)
+       +(3*sd(imiss_het$Het_propo)),col="RED",lty=2)
+##Missing Data Thresholds (Vertical Line)
+abline(v=0.03, col="BLUE", lty=2) # THRESHOLD=missing_rate
+title("Distribution of missing and heterozygosity rate")
+
+m1<-mean(imiss_het$Het_propo)-(3*sd(imiss_het$Het_propo))
+m2<-mean(imiss_het$Het_propo)+(3*sd(imiss_het$Het_propo))
+
+if (length(imiss_het[imiss_het$F_MISS>0.03 |imiss_het$Het_propo>m2 |imiss_het$Het_propo<m1,'FID'])!=0){
+text(x=imiss_het[imiss_het$F_MISS>0.03 |imiss_het$Het_propo>m2 |imiss_het$Het_propo<m1,'F_MISS' ],y=imiss_het[imiss_het$F_MISS>0.03 |imiss_het$Het_propo>m2 |imiss_het$Het_propo<m1,'Het_propo' ],labels=imiss_het[imiss_het$F_MISS>0.03 |imiss_het$Het_propo>m2 |imiss_het$Het_propo<m1,'FID'],cex=0.9, font=2, pos=4)
+}
+dev.off()
+
+
+imiss <- read.table("/Users/kenny/Desktop/Laptop_input/target_miss_indQC.imiss", header = T, sep = "")
+het <- read.table("/Users/kenny/Desktop/Laptop_input/target_het_indQC.het", header = T, sep ="")
+##visulize individals fail imiss and het
+het$Het_propo = (het$N.NM. - het$O.HOM.)/het$N.NM.
+imiss_het = merge(het,imiss,by = "FID")
+###make a plot
+Addcolors = densCols(imiss_het$F_MISS,imiss_het$Het_propo)
+
+tiff(filename = '/Users/kenny/Desktop/Laptop_output/OutputImage/QCplot/indQCplot_target.tiff',height=6, width=8,units='in', res=600)
+
+indQCplot<-plot(imiss_het$F_MISS,imiss_het$Het_propo, col=Addcolors,pch=20,
+     xlim=c(0,0.06),
+     ylim=c(0.18,0.212),
+     #ylim=c(0.1,0.35),
+     xlab="Missing rate", ylab="Heterozygosity rate", axes=FALSE, cex.lab=1.3, cex.axis=1.3, cex.main=1.3)
+axis(2,tick=T) #2, left. t, tickmarks
+axis(1,tick=T) #1, below
+legend("bottomright", legend="Genotypes_missing_het", pch=16, bty="n", col = Addcolors)
+abline(h=mean(imiss_het$Het_propo)-
+         (3*sd(imiss_het$Het_propo)),col="RED",lty=2)
+abline(h=mean(imiss_het$Het_propo)
+       +(3*sd(imiss_het$Het_propo)),col="RED",lty=2)
+##Missing Data Thresholds (Vertical Line)
+abline(v=0.03, col="BLUE", lty=2) # THRESHOLD=missing_rate
+title("Distribution of missing and heterozygosity rate")
+
+m1<-mean(imiss_het$Het_propo)-(3*sd(imiss_het$Het_propo))
+m2<-mean(imiss_het$Het_propo)+(3*sd(imiss_het$Het_propo))
+
+if (length(imiss_het[imiss_het$F_MISS>0.03 |imiss_het$Het_propo>m2 |imiss_het$Het_propo<m1,'FID'])!=0){
+text(x=imiss_het[imiss_het$F_MISS>0.03 |imiss_het$Het_propo>m2 |imiss_het$Het_propo<m1,'F_MISS' ],y=imiss_het[imiss_het$F_MISS>0.03 |imiss_het$Het_propo>m2 |imiss_het$Het_propo<m1,'Het_propo' ],labels=imiss_het[imiss_het$F_MISS>0.03 |imiss_het$Het_propo>m2 |imiss_het$Het_propo<m1,'FID'],cex=0.9, font=2, pos=4)
+}
+
+dev.off()
